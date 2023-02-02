fddcbe1969722110d61b7819cff55fc48d154487
devthalal <thalal.hassan@neoito.com>
2023-02-02 12:08:36 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const test_b = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello test_b`}))
+  res.end()
+  
+}
+
+export default test_b
